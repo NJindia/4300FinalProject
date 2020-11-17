18425825534fb2163fc3873606caea8e033d2e06
Alexander Nguyen <nguyen.alex22@gmail.com>
2020-11-16 23:35:34 -0500

    made changes in code to address certain test cases

---
+++ editEmailAndPassPHP.php
@@ -0,0 +1,28 @@
+<?php
+
+include('database.php');
+$userID = filter_input(INPUT_POST, 'userID', FILTER_VALIDATE_INT);
+
+$query = "SELECT password FROM user_info WHERE id = '1' ";
+$info = $db->query($query);
+$info = $info->fetch();
+
+$email=$_POST['email'];
+$currentPass=$_POST['currentPass'];
+$newPass=$_POST['newPass'];
+
+
+
+if ($currentPass == $info['password']) {
+    if($email == "") {
+        $query="UPDATE user_info SET password = '$newPass' WHERE id = '1'" ;
+        $db->exec($query);
+    }
+    else {
+        $query="UPDATE user_info SET email = '$email', password = '$newPass' WHERE id = '1'" ;
+        $db->exec($query);
+    }
+}
+
+include('myAccount.php');
+?>
