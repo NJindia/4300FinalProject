e1e49ddbb7579dd8985c71cc73825b974e6f28ec
Alexander Nguyen <nguyen.alex22@gmail.com>
2020-11-22 18:47:03 -0500

    Process Registration fixes

--- process_registration.php
+++ process_registration.php
@@ -1,5 +1,5 @@
 <?php
-    include('database.php');
+include('database.php');
 
 $first = $_POST['first'];
 $last = $_POST['last'];
@@ -15,155 +15,125 @@ $cardNumber = $_POST['cardNumber'];
 $expiration = $_POST['expiration'];
 
 
-// if (empty($first)) {
-//         $first_error = "Please input a name";       
-// }
-// else {
-//         $query = "INSERT INTO user_info (first, last, email, password, phone) VALUES ('$first', '$last', '$email', '$phone', '$password')";
-//         $db->exec($query);    
-// }
-if (!empty($email)) {
-        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-            $email_error = "Please input a valid E-Mail Address.";
-        } else {
-            $query = "INSERT INTO user_info SET email = '$email'";
-            $db->exec($query);
-        }
-    }
-    if (!empty($password)) {
-        if (!preg_match('/^(?=.*[!@#$%^&*-])(?=.*[0-9])(?=.*[A-Z]).{8,20}$/', $password)) {
-            $newPass_error = "Please input a valid New Password.<br>
-            A password must be 8-20 characters long with at least: 1 Uppercase, 1 number, and 1 special character.";
-        } else {
-            $query = "INSERT INTO user_info SET password = '$password'";
-            $db->exec($query);
-        }
-    }
 
+//--------personal info--------
 if (!empty($first)) {
     if (!preg_match("/^[a-zA-Z]+$/", $first)) {
         $first_error = "Please input a valid first name.";
-    } else {
-        $query = "INSERT INTO user_info SET first = '$first'";
-        $db->exec($query);
     }
+} else {
+    $first_error = "Please input a valid first name.";
 }
+
 if (!empty($last)) {
     if (!preg_match("/^[a-zA-Z]+$/", $last)) {
         $last_error = "Please input a valid last name.";
-    } else {
-        $query = "INSERT INTO user_info SET last = '$last'";
-        $db->exec($query);
     }
+} else {
+    $last_error = "Please input a valid last name.";
 }
+
 if (!empty($phone)) {
     if (!preg_match("/^[0-9]{10}$/", $phone)) {
         $phone_error = "Please input a valid phone number.";
-    } else {
-        $query = "INSERT INTO user_info SET phone = '$phone'";
-        $db->exec($query);
     }
+} else {
+    $phone_error = "Please input a valid phone number.";
 }
 
-
-
-// $query = "INSERT INTO payment (card_type, card_num, expiration) VALUES ('$cardType', '$cardNumber', '$expiration')";
-//         $db->exec($query);
-
-if (!empty($cardNumber)) {
-    if (!preg_match("/^[0-9]{16}$/",$cardNumber)) {
-        $card_num_error = "Please enter a valid Card Number.";
+//--------email and pass info--------
+if (!empty($email)) {
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        $email_error = "Please input a valid E-Mail Address.";
     }
-    else { 
-        $query = "INSERT INTO payment SET card_num = '$cardNumber'";
-        $db->exec($query);
+} else {
+    $email_error = "Please input a valid E-Mail Address.";
+}
+if (!empty($password)) {
+    if (!preg_match('/^(?=.*[!@#$%^&*-])(?=.*[0-9])(?=.*[A-Z]).{8,20}$/', $password)) {
+        $newPass_error = "Please input a valid New Password.<br>
+         A password must be 8-20 characters long with at least: 1 Uppercase, 1 number, and 1 special character.";
     }
-}
-else {
-    $card_num_error = "Please enter a valid Card Number.";
-}
-function validateDate($date, $format= 'Y-m-d'){
-    return $date == date($format, strtotime($date));
-}
-if (!empty($expiration)) {
-    if (!validateDate($expiration)){
-        $expiration_error = "Please enter a valid Expiration Date.";
-    }
-    else {
-            $query = "INSERT INTO payment SET expiration = '$expiration'";
-        $db->exec($query);
-    }
-}
-else {
-    $expiration_error = "Please enter a valid Expiration Date.";
+} else {
+    $newPass_error = "Please input a valid New Password.<br>
+         A password must be 8-20 characters long with at least: 1 Uppercase, 1 number, and 1 special character.";
 }
 
-        
-//  $query = "INSERT INTO address (street, city, state, zipcode) VALUES ('$street', '$city', '$state', '$zip')";
-//         $db->exec($query);
-//         echo $first;       
-
-
-        if (!empty($street)) {
-    if (!preg_match("/^[a-zA-Z0-9 ]+$/",$street)) {
+//--------Address info--------
+if (!empty($street)) {
+    if (!preg_match("/^[a-zA-Z0-9 ]+$/", $street)) {
         $street_error = "Please input a valid Street name.";
     }
-    else {
-        $query = "INSERT INTO address SET street = '$street'";
-        $db->exec($query);
-    }
+} else {
+    $street_error = "Please input a valid Street name.";
 }
 
 if (!empty($city)) {
-    if (!preg_match("/^[a-zA-Z]+$/",$city)) {
+    if (!preg_match("/^[a-zA-Z]+$/", $city)) {
         $city_error = "Please input a valid City.";
     }
-    else {
-        $query = "INSERT INTO address SET city = '$city'";
-        $db->exec($query);
-    }
+} else {
+    $city_error = "Please input a valid City.";
 }
 
 if (!empty($state)) {
-    if (!preg_match("/^[a-zA-Z]+$/",$state)) {
+    if (!preg_match("/^[a-zA-Z]+$/", $state)) {
         $state_error = "Please input a valid State.";
     }
-    else {
-        $query = "INSERT INTO address SET state = '$state'";
-        $db->exec($query);
-    }
+} else {
+    $state_error = "Please input a valid State.";
 }
 
 if (!empty($zip)) {
-    if (!preg_match("/^[0-9]{5}$/",$zip)) {
+    if (!preg_match("/^[0-9]{5}$/", $zip)) {
         $zipcode_error = "Please input a valid Zipcode.";
     }
-    else {
-        $query = "INSERT INTO address SET zip VALUES '$zip'";
-        $db->exec($query);
-    }
-}
-if (empty($street_error) && empty($city_error) && empty($state_error) && empty($zipcode_error) && empty($first_error) && 
-empty($email_error) && empty($currentPass_error) && empty($newPass_error) && empty($last_error) && empty($phone_error)
-&& empty($card_type_error) && empty($card_num_error) && empty($expiration_error)) {
-    $query = "INSERT INTO user_info (first, last, email, password, phone) VALUES ('$first', '$last', '$email', '$phone', '$password')";
-    $db->exec($query);
-        $query = "SELECT user_id FROM user_info WHERE user_id = (SELECT Max(user_id) FROM user_info)";
-        $lastID = $db->query($query);
-        
-    $query = "INSERT INTO address (user_id,street, city, state, zipcode) VALUES ('$lastID', '$city', '$state', '$zip')";
-    $db->exec($query);  
-    $query = "INSERT INTO payment (user_id,card_type, card_num, expiration) VALUES ('$lastID', '$cardNumber', '$expiration')";
-    $db->exec($query);
-
-
-        header("location:login.php");
+} else {
+    $zipcode_error = "Please input a valid Zipcode.";
 }
 
-else {
+//---------payment info---------
+if (!empty($cardNumber)) {
+    if (!preg_match("/^[0-9]{16}$/", $cardNumber)) {
+        $card_num_error = "Please enter a valid Card Number.";
+    } 
+} else {
+    $card_num_error = "Please enter a valid Card Number.";
+}
+function validateDate($date, $format = 'Y-m-d')
+{
+    return $date == date($format, strtotime($date));
+}
+if (!empty($expiration)) {
+    if (!validateDate($expiration)) {
+        $expiration_error = "Please enter a valid Expiration Date.";
+    } 
+} else {
+    $expiration_error = "Please enter a valid Expiration Date.";
+}
+
+
+if (
+    empty($street_error) && empty($city_error) && empty($state_error) && empty($zipcode_error) && empty($first_error) &&
+    empty($email_error) && empty($currentPass_error) && empty($newPass_error) && empty($last_error) && empty($phone_error)
+    && empty($card_type_error) && empty($card_num_error) && empty($expiration_error)
+) {
+    $query = "INSERT INTO user_info (first, last, email, password, phone) VALUES ('$first', '$last', '$email', '$password', '$phone')";
+    $db->exec($query);
+
+    $query = "SELECT id FROM user_info WHERE email = '$email'";
+    $info = $db->query($query);
+    $info = $info->fetch();
+
+    $id = $info['id'];
+
+    $query = "INSERT INTO address (user_id, street, city, state, zipcode) VALUES ('$id', '$street','$city', '$state', '$zip')";
+    $db->exec($query);
+    $query = "INSERT INTO payment (user_id, card_type, card_num, expiration) VALUES ('$id', '$cardType', '$cardNumber', '$expiration')";
+    $db->exec($query);
+
+
+    header("location:login.php");
+} else {
     include("registration.php");
 }
-
-
-
-?>
