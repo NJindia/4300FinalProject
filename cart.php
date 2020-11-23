076dbc9f37cb2e4cd6745d393d95bf0f5d856355
jae Oh <jayoh28@uga.edu>
2020-11-22 23:03:30 -0500

    cary page

--- cart.php
+++ cart.php
@@ -1,5 +1,6 @@
 <?php
 require_once('database.php');
+SESSION_start();
 $user_id = 1;
 $itemsQuery = 'SELECT * FROM cart WHERE user_id = :user_id';
 $statement = $db->prepare($itemsQuery);
@@ -27,17 +28,17 @@ $statement->closeCursor();
         <a href="home.php"><img id="title_logo" src="images/title.png"></a><br>
         <nav id="text_nav" class="top_nav">
             <ul>
-                <li class="li_left" id="currPage"><a href="home.php">Home</a></li>
+                <li class="li_left" ><a href="home.php">Home</a></li>
                 <li class="li_left"><a href="about_us.php">About Us</a></li>
                 <li class="li_left"><a href="contact_us.php">Contact Us</a></li>
                 <li class="li_right"><img id="pfp" src="images/profilepic.png">
                     <ul>
                         <li><a href="myAccount.php">My Account</a></li>
-                        <li><a href="logout.php">Log Out</a></li>
+                        <li><a href="logout.php" >Log Out</a></li>
                     </ul>
                 </li>
                 <?php
-                if (isset($_SESSION['first'])) { ?><li class="li_right"><a href="cart.php"><img id="cart" src="images/cart.png"></a></li><?php } ?>
+                if (isset($_SESSION['first'])) { ?><li class="li_right" id="currPage"><a href="cart.php" ><img id="cart" src="images/cart.png" ></a></li><?php } ?>
             </ul>
         </nav>
         <div id="cart">
@@ -81,6 +82,15 @@ $statement->closeCursor();
             </table>
         </div>
         <a href="checkout.php"><img id="checkout" src="images/checkout.png"></a>
+        <div id="social_media">
+            <a href="#" ><img class="social_media" src="images/discord.png"></a>
+            <a href="#" ><img class="social_media" src="images/reddit.png"></a>
+            <a href="#" ><img class="social_media" src="images/twitter.png"></a>
+            <a href="#" ><img class="social_media" src="images/instagram.png"></a>
+            <br>
+            <p>&copy; Smoke Games</p>
+        </div>
+        </div>
     </div>
 </body>
 
