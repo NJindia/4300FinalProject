4d7325c5a7f7d73ec9af218a9c8ca65c3033609e
Nikhil Jindia <nikhiljindia@gmail.com>
2020-11-22 22:52:48 -0500

    another one

--- about_us.php
+++ about_us.php
@@ -14,7 +14,7 @@
             <a href="home.php"><img id="title_logo" src="images/title.png"></a><br>
             <nav id="text_nav" class="top_nav">
                 <ul>
-                    <li class="li_left" ><a href="home.php">Home</a></li>
+                    <li class="li_left"><a href="home.php">Home</a></li>
                     <li class="li_left" id="currPage"><a href="about_us.php">About Us</a></li>
                     <li class="li_left"><a href="contact_us.php">Contact Us</a></li>
                     <li class="li_right"><img id="pfp" src="images/profilepic.png">
@@ -57,11 +57,10 @@
                         </tr>
                     </table>
                     <div id="text">
-                        <p id="intro">Hi there, fellow humans! We created this website to act as an Indie Game Developer's video game digital distribution service. We created three mock-up games that do NOT exist. Our team consists of junioir and senior CS students as UGA. We hope you enjoy your stay and if you have any questions, please <a href="contact_us.php">contact us!</a></p>
+                        <p id="intro">Hi there, fellow humans! We created this website to act as an Indie Game Developer's video game digital distribution service. We created three mock-up games that do NOT exist and are not available for purchase. Our team consists of junior and senior CS students as UGA. We hope you enjoy your stay and if you have any questions, please <a href="contact_us.php">contact us!</a></p>
                         <h1>Mission Statement</h1>
-                        <p>We strive to earn an A on our final project and we hope that our website is worthy of such grade. Our goal is to continue making games that
-                            is suitable for all players of any age. We value our customers, contributors, and developers, and we will continue to show this image of ourselves.
-                            Thank you for your support!
+                        <p>We strive to earn an A on our final project and we hope that our website is worthy of such grade. Our goal is to continue making games that is suitable for all players of any age. We value our customers, contributors, and developers, and we will continue to show this image of ourselves.
+                        Thank you for your support!
                         </p>
                         <h1>Disclaimer</h1>
                         <p>Our company and our offered games are works of fiction and do not exist in the real world. We are not associated with any real company.
